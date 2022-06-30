9b62b0280cd62dabf329d1efdd3d011000509f3e
Satyam Pahariya <100020434+Satyam-Pahariya@users.noreply.github.com>
2022-06-30 18:52:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+// jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname+"/date.js");
+
+const app = express();
+const items = ["Buy something", "Cook something"];
+const workItems = [];
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public")); 
+
+app.get ("/", function(req,res){
+const  day = date.getDate(); 
+  res.render("list", {listTitle:day, newListItems: items});
+
+});
+app.post("/", function(req, res){
+const item = req.body.newItem;
+
+  if(req.body.list === "Work list"){
+    workItems.push(item);
+    res.redirect ("/work");
+  }else{
+    items.push(item);
+    res.redirect ("/");
+  }
+});
+app.get("/work", function(req,res){
+  res.render("list", {listTitle:"Work list", newListItems:workItems});
+});
+
+app.post("/work", function(req,res){
+  const item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
+
+
+app.get("/about", function(req,res){
+  res.render("about");
+});
+
+
+app.listen(3000, function(){
+console.log("Server is running on port 3000");
+});
